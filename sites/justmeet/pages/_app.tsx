da999d4928487a654df9581022f2a1354d6c72b4
Henry Gressmann <mail@henrygressmann.de>
2022-02-05 00:34:36 +0100

    chore: upgrade deps
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- sites/justmeet/pages/_app.tsx
+++ sites/justmeet/pages/_app.tsx
@@ -1,15 +1,10 @@
 import { FC } from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { Layout } from '../components/layout';
 import { SupabaseContext } from '../hooks/supabase';
 
-const queryClient = new QueryClient();
-
 const App: FC = ({ children }) => (
 	<Layout>
-		<QueryClientProvider client={queryClient}>
-			<SupabaseContext>{children}</SupabaseContext>
-		</QueryClientProvider>
+		<SupabaseContext>{children}</SupabaseContext>
 	</Layout>
 );
 export default App;
